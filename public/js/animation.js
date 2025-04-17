635eb78f5ee8a76089682498b7d545b0e6a6d34f
rogo! <r480github@gmail.com>
2025-04-16 20:57:00 -0400

    finish homepage

---
+++ public/js/animation.js
@@ -0,0 +1,95 @@
+gsap.fromTo(
+  ".indextitle1",
+  {
+    opacity: 0,
+    x: -40,
+  },
+  {
+    opacity: 1,
+    x: 0,
+    duration: 1.2,
+    ease: "expo.inOut",
+  }
+);
+gsap.fromTo(
+    ".indextitle2",
+    {
+      opacity: 0,
+      y: -20,
+    },
+    {
+      delay: 0.3,
+      opacity: 1,
+      y: 0,
+      duration: 1.2,
+      ease: "expo.inOut",
+    }
+  );
+  gsap.fromTo(
+    ".indextitle3",
+    {
+      opacity: 0,
+      y: 30,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 1.2,
+      ease: "expo.inOut",
+    }
+  );
+  gsap.fromTo(
+    ".indextitle4",
+    {
+      opacity: 0,
+      y: -50,
+    },
+    {
+      delay: 0.1,
+      opacity: 1,
+      y: 0,
+      duration: 1.2,
+      ease: "expo.inOut",
+    }
+  );
+  gsap.fromTo(
+    ".indextitle5",
+    {
+      opacity: 0,
+      x: 30,
+    },
+    {
+      opacity: 1,
+      x: 0,
+      duration: 1.2,
+      ease: "expo.inOut",
+    }
+  );
+  gsap.fromTo(
+    ".indextitle6",
+    {
+      opacity: 0,
+      y: -30,
+    },
+    {
+      delay: 0.4,
+      opacity: 1,
+      y: 0,
+      duration: 1.2,
+      ease: "expo.inOut",
+    }
+  );
+  gsap.fromTo(
+    ".indextitle7",
+    {
+      opacity: 0,
+      x: -30,
+    },
+    {
+      delay: 0.1,
+      opacity: 1,
+      x: 0,
+      duration: 1.2,
+      ease: "expo.inOut",
+    }
+  );
